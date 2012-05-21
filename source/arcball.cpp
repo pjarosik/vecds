a6f7e1190c7adfdf60da51b74332f1fb05bd5a68
oneliefleft <oneliefleft@46551c14-c27e-41b6-87a3-467ec993ed05>
2012-05-21 11:55:43 +0000

    Make arcball more strict on what it can do.
    git-svn-id: https://svn.code.sf.net/p/vecds/code@49 46551c14-c27e-41b6-87a3-467ec993ed05

--- source/arcball.cpp
+++ source/arcball.cpp
@@ -22,68 +22,93 @@
 //					 
 // -------------------------------------------------------------------
 
-#include "../include/vecds/arcball.h" // ArcBall header
 
-//Arcball sphere constants:
-//Diameter is       2.0f
-//Radius is         1.0f
-//Radius squared is 1.0f
+                                 // vecds includes
+#include <vecds/arcball.h> 
 
-QVector3D ArcBall::mapToSphere (QVector2D mousePt)
-{
-  QVector3D res  = QVector3D (mousePt.x(), mousePt.y(), 0.);
-  double length2 = res.x()*res.x() + res.y()*res.y();
-  
-  if ( length2>1.96 ) 
-    {
-      double norm = sqrt(length2);
-      res = QVector3D (res.x()/norm, res.y()/norm, 0.);
-    } 
-  else 
-    res.setZ (sqrt(1.96-length2));
-  
-  return res;
-}
 
-/**
- * Constructor. Create an empty ArcBall.
- */
+                                 /* Constructor. Create an empty
+				    (null) ArcBall by initialisation
+				    of certain variables. */
 ArcBall::ArcBall () 
-{
-  this->epsilon  = 1.e-8;
-  
-  this->startVec = this->endVec = QVector3D(0., 0., 0.);
-  
-  this->mouseQuat    = 
-    this->q_down     = 
-    this->quaternion = 
-    QQuaternion (1., 0., 0., 0.);
-}
+  :
+  epsilon    (1.e-8),
+  vector_begin   (QVector3D (0., 0., 0.)),
+  vector_end     (QVector3D (0., 0., 0.)),
+  mouseQuat  (QQuaternion (1., 0., 0., 0.)),
+  q_down     (QQuaternion (1., 0., 0., 0.)),
+  quaternion (QQuaternion (1., 0., 0., 0.))    
+{}
 
-void ArcBall::click (QVector2D mousePt)
+ArcBall::~ArcBall () 
+{}
+
+void ArcBall::click (const QVector2D &mouse_coordinate)
 {
-  this->startVec  = mapToSphere(mousePt);
+  this->vector_begin  = mapToSphere (mouse_coordinate);
   this->q_down    = this->quaternion;
-  this->mouseQuat = QQuaternion(1., 0., 0., 0.);
+  this->mouseQuat = QQuaternion (1., 0., 0., 0.);
 }
 
-void ArcBall::drag (QVector2D mousePt)
+void ArcBall::drag (const QVector2D &mouse_coordinate)
 {
-  this->endVec     = mapToSphere(mousePt);
-  this->mouseQuat  = get_quaternion(this->startVec, this->endVec);
+  this->vector_end     = mapToSphere (mouse_coordinate);
+  this->mouseQuat  = get_quaternion (this->vector_begin, this->vector_end);
   this->quaternion = this->mouseQuat * this->q_down;
 }
 
-QQuaternion ArcBall::get_quaternion (QVector3D v1, QVector3D v2)
+QQuaternion ArcBall::get_quaternion (const QVector3D &vec_1, 
+				     const QVector3D &vec_2)
 {
-  QVector3D perp = QVector3D::crossProduct (v2, v1);
-  return (perp.x()*perp.x() + 
-	  perp.y()*perp.y() +
-	  perp.z()*perp.z() > 
+  QVector3D perpendicular_vector = QVector3D::crossProduct (vec_2, vec_1);
+
+  return (perpendicular_vector.x ()*perpendicular_vector.x () + 
+	  perpendicular_vector.y ()*perpendicular_vector.y () +
+	  perpendicular_vector.z ()*perpendicular_vector.z () > 
 	  this->epsilon)   
     ?
-    QQuaternion (QVector3D::dotProduct(v1, v2), 
-		 perp.x(), perp.y(), perp.z()).normalized() 
+    QQuaternion (QVector3D::dotProduct (vec_1, vec_2), 
+		 perpendicular_vector.x (), 
+		 perpendicular_vector.y (), 
+		 perpendicular_vector.z ()
+		 ).normalized () 
     :
-    QQuaternion(1., 0., 0., 0.);
+    QQuaternion (1., 0., 0., 0.);
+}
+
+
+QVector3D ArcBall::mapToSphere (QVector2D mouse_coordinate)
+{
+  const double length = 
+    mouse_coordinate.x ()*mouse_coordinate.x () + 
+    mouse_coordinate.y ()*mouse_coordinate.y ();
+  
+                                 // If the length comes back as a
+                                 // negative number we are in trouble,
+                                 // check that and bail out if
+                                 // length!>=0.
+  assert (length>0);
+
+                                 // The mouse_coordinate is in 2d (ie,
+                                 // it is on the screen), byt we need
+                                 // a 3d object. We therefore compute
+                                 // the "mouse_position" on the
+                                 // surface of a sphere.
+  QVector3D mouse_position;
+
+                                 // TODO: What is 1.96? Why?
+  if (length>1.96) 
+    {
+      double l2_norm = sqrt (length);
+      mouse_position = QVector3D (mouse_coordinate.x ()/l2_norm, 
+				  mouse_coordinate.y ()/l2_norm, 0.);
+    } 
+  else 
+    {
+      mouse_position = QVector3D (mouse_coordinate.x (), 
+				  mouse_coordinate.y (), 
+				  sqrt (1.96-length));
+    }
+  
+  return mouse_position;
 }
