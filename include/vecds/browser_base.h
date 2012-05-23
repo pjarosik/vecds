6f4e8e526d5abfa74d5563cccc82c3411cca0b34
oneliefleft <oneliefleft@46551c14-c27e-41b6-87a3-467ec993ed05>
2012-05-23 13:55:18 +0000

    Skeleton start for a virtual browser base.
    git-svn-id: https://svn.code.sf.net/p/vecds/code@71 46551c14-c27e-41b6-87a3-467ec993ed05

---
+++ include/vecds/browser_base.h
@@ -0,0 +1,83 @@
+
+// -------------------------------------------------------------------
+//
+// Author: Toby D. Young (2012)
+//
+// Copyright (C) 2012 The vecds authors
+//
+// This program is free software: you can redistribute it and/or
+// modify  it under the  terms of  the GNU  General Public  License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//  
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have  received a copy of the  GNU General Public License
+// along with this program.  If not, see
+// <http://www.gnu.org/licenses/>.
+//					 
+// -------------------------------------------------------------------
+
+#ifndef BROWSER_BASE_H
+#define BROWSER_BASE_H
+
+                                 /* first the usual set of Qt crap */
+#include <QtGui>
+#include <QWidget> 
+
+                                 /* vecds includes */
+#include <vecds/config.h>
+
+                                 /* forward declarations of qt
+				    clases */
+class QPushButton;
+class QTextBrowser;
+
+namespace vecds
+{
+
+                                 /* This class represents a basic
+				    browser base from which other
+				    browser classes can be
+				    constructed. The idea here is to
+				    create a window that displays some
+				    text and maybe some figures too
+				    based (mostly) on html input
+				    files.  */
+  class BrowserBase 
+    : 
+  public QWidget
+  {
+    Q_OBJECT 
+      
+      public: 
+  
+                                 /* constructor of the main clas, just
+				    create a pointer to the parent */
+    BrowserBase (QWidget *parent = 0);
+  
+                                 /* destructor of the main class */
+    ~BrowserBase ();
+  
+  private: 
+
+                                 /* TODO: some kind of parent
+				    widget(?) */
+    QWidget      *parent_widget;
+
+                                 /* the browser itself */
+    QTextBrowser *text_browser;
+
+                                 /* string denoting a generic path */
+    QString       path;
+
+                                 /* standard browser-type buttons */
+    QPushButton  *close_button;
+  };                             /* BrowserBase */
+
+}                                /* namespace vecds */
+
+#endif
---
+++ source/browser_base.cxx
@@ -0,0 +1,42 @@
+
+// -------------------------------------------------------------------
+//
+// Author: Toby D. Young (2012)
+//
+// Copyright (C) 2012 The vecds authors
+//
+// This program is free software: you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//  
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see
+// <http://www.gnu.org/licenses/>.
+//					 
+// -------------------------------------------------------------------
+
+
+#include <iostream>
+
+                                 // vecds includes
+#include <vecds/browser_base.h>
+
+                                 // pomocnicza klasa - przeglądarka
+                                 // plików pomocy (HTML).
+
+                                 // constructor
+vecds::BrowserBase::BrowserBase (QWidget *parent)
+  :
+  parent_widget (parent),
+  path (VECDS_DOCS)
+{}
+
+                                 // constructor
+vecds::BrowserBase::~BrowserBase ()
+{}
